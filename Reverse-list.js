e72954ef9795851e5047b36fcb21f0499e61f7a6
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-18 16:06:22 -0700

    Update Reverse-list.js

--- Reverse-list.js
+++ Reverse-list.js
@@ -4,3 +4,11 @@ Write reverseList function that simply reverses lists.
 
 
 // Solution
+
+function reverseList(arr) {
+  let res = [];
+  for(let i of arr) {
+    res.unshift(i);
+  }
+  return res;
+}
